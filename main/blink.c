525613203f833c63fa3170ce60951403df809b33
Matius Chairani <matius.chairani@tum.de>
2022-05-13 11:43:03 +0200

    Show room state with stupid global counter logic

--- main/blink.c
+++ main/blink.c
@@ -16,69 +16,69 @@
 #define BLINK_FREQ CONFIG_BLINK_FREQ
 
 static const char *TAG = "BLINK";
-static const char *L1 = "LIGHT1";
-static const char *L2 = "LIGHT2";
-static const uint8_t eventQueueLength = 5;
+// static const char *L1 = "LIGHT1";
+// static const char *L2 = "LIGHT2";
 
 volatile uint8_t count = 0;
-static QueueHandle_t eventQueue;
+static TaskHandle_t taskHandle;
 
 void initDisplay() {
-	ssd1306_128x32_i2c_init();
-	ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_128x32_i2c_init();
+    ssd1306_setFixedFont(ssd1306xled_font6x8);
+    ssd1306_clearScreen();
 }
 
-void queueTest(void* pvParameters) {
-	int item;
-	while (1) {
-		if (xQueueReceive(eventQueue, (void*) &item, 10) != pdTRUE) {
-			// Throw error message
-		} else {
-			ESP_LOGI(TAG, "Received message from Light %d", item);
-		}
-	}
+void printToDisplay(int number) {
+    char buffer[1];
+ 	itoa((int) number, buffer, 10);
+ 	ssd1306_printFixedN(0, 0, "Count:", STYLE_NORMAL, 1);
+ 	ssd1306_printFixedN(0, 16, buffer, STYLE_NORMAL, 1);
+}
+
+void showRoomState(void* pvParameters) {
+    uint8_t lastCount = -1;
+    while(1) {
+        if (lastCount != count) {
+            lastCount = count;
+            printToDisplay(count);
+        }
+    }
 }
 
 void light1Handler() {
-	xQueueSendToBackFromISR(eventQueue, 1, 10);
+    count++;
 }
 
 void light2Handler() {
-	xQueueSendToBackFromISR(eventQueue, 2, 10);
+    count--;
 }
 
 void app_main(void){
-	esp_log_level_set("BLINK", ESP_LOG_INFO);
+    esp_log_level_set("BLINK", ESP_LOG_INFO);
 
-	/* HW Setup */
+    /* HW Setup */
 
-	ESP_ERROR_CHECK(gpio_set_direction(BLINK_GPIO, GPIO_MODE_INPUT_OUTPUT));	// set pin 19 as input and output
-	ESP_ERROR_CHECK(gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT));			// set pin 23 as input
-	ESP_ERROR_CHECK(gpio_set_direction(LIGHT1_GPIO, GPIO_MODE_INPUT));			// set pin 18 as input
-	ESP_ERROR_CHECK(gpio_set_direction(LIGHT2_GPIO, GPIO_MODE_INPUT));			// set pin 26 as input
+    ESP_ERROR_CHECK(gpio_set_direction(BLINK_GPIO, GPIO_MODE_INPUT_OUTPUT));    // set pin 19 as input and output
+    ESP_ERROR_CHECK(gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT));          // set pin 23 as input
+    ESP_ERROR_CHECK(gpio_set_direction(LIGHT1_GPIO, GPIO_MODE_INPUT));          // set pin 18 as input
+    ESP_ERROR_CHECK(gpio_set_direction(LIGHT2_GPIO, GPIO_MODE_INPUT));          // set pin 26 as input
 
-	/* Queue */
+    /* Display */
 
-	eventQueue = xQueueCreate(eventQueueLength, 5 * sizeof(uint8_t));
+    initDisplay();
 
-	/* Display */
+    xTaskCreate(
+        showRoomState,
+        "showRoomState",
+        1024,
+        NULL,
+        1,
+        taskHandle
+    );
 
-	initDisplay();
-
-	xTaskCreate(
-			queueTest,
-			"ShowRoomState",
-			1024,
-			NULL,
-			1,
-			NULL);
-
-	
-	/* HW Interrupts */
-
-	gpio_install_isr_service(0);
-	gpio_set_intr_type(LIGHT1_GPIO, GPIO_INTR_NEGEDGE);
-	gpio_isr_handler_add(LIGHT1_GPIO, light1Handler, NULL);
-	gpio_set_intr_type(LIGHT2_GPIO, GPIO_INTR_NEGEDGE);
-	gpio_isr_handler_add(LIGHT2_GPIO, light2Handler, NULL);
+    gpio_install_isr_service(0);
+ 	gpio_set_intr_type(LIGHT1_GPIO, GPIO_INTR_NEGEDGE);
+ 	gpio_isr_handler_add(LIGHT1_GPIO, light1Handler, NULL);
+ 	gpio_set_intr_type(LIGHT2_GPIO, GPIO_INTR_NEGEDGE);
+ 	gpio_isr_handler_add(LIGHT2_GPIO, light2Handler, NULL);
 }
