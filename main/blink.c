9753f7aa8bc915186d9018fdb59bbd900b4110d5
Luca Favaro <79704017+lucafavaro1@users.noreply.github.com>
2022-05-16 10:38:03 +0200

    Update blink.c
    Working version + commented updates

--- main/blink.c
+++ main/blink.c
@@ -8,21 +8,27 @@
 #include "esp_timer.h"
 #include "ssd1306.h"
 
+
 #define BLINK_GPIO CONFIG_BLINK_GPIO	// led -> 19
 #define BUTTON_GPIO CONFIG_BUTTON_GPIO // button -> 23
 #define LIGHT1_GPIO CONFIG_LIGHT1_GPIO		// light 1 -> 18
 #define LIGHT2_GPIO CONFIG_LIGHT2_GPIO		// light 2 -> 26
 #define BLINK_LENGTH CONFIG_BLINK_LENGTH
 #define BLINK_FREQ CONFIG_BLINK_FREQ
+#define DEBOUNCE_TIME 0.5
 
 static const char *TAG = "BLINK";
-// static const char *L1 = "LIGHT1";
-// static const char *L2 = "LIGHT2";
 
 volatile uint8_t count = 0;
-bool oldStateLight1, oldStateLight2, stateChanged;
+bool oldStateLight1 = false, oldStateLight2 = false;
+int countLight1 = 0, countLight2 = 0;
 
-static TaskHandle_t taskHandle;
+int lastFlickerableState1 = LOW, lastFlickerableState2 = LOW;
+
+unsigned long lastDebounceTime1 = 0, lastDebounceTime2 = 0;
+
+
+static IRAM_ATTR TaskHandle_t taskHandle;
 
 void initDisplay() {
     ssd1306_128x32_i2c_init();
@@ -30,22 +36,26 @@ void initDisplay() {
     ssd1306_clearScreen();
 }
 
-void printToDisplay(int number) {
+void IRAM_ATTR printToDisplay(int number) {
     char buffer[1];
     ssd1306_clearScreen();
  	itoa((int) number, buffer, 10);
  	ssd1306_printFixedN(0, 0, "Count:", STYLE_NORMAL, 1);
+	ssd1306_printFixedN(96, 0, "G-10", STYLE_NORMAL, 0.5);
  	ssd1306_printFixedN(0, 16, buffer, STYLE_NORMAL, 1);
 }
 
-void showRoomState(void* pvParameters) {
+// Version 1: cross both the sensors to get counted
+void IRAM_ATTR showRoomState(void* pvParameters) {
     int lastCount = -1;
     bool stateLight1, stateLight2;
-    oldStateLight1 = gpio_get_level(LIGHT1_GPIO);
-    oldStateLight2 = gpio_get_level(LIGHT2_GPIO);
-    while(1) {
+
+	uint8_t ulNotifiedValue;
+    
+	while(1) {
         stateLight1 = gpio_get_level(LIGHT1_GPIO);
         stateLight2 = gpio_get_level(LIGHT2_GPIO);
+
         if (stateLight1 != oldStateLight1 && stateLight1) {
             if (stateLight2) {
                 count++;
@@ -58,26 +68,86 @@ void showRoomState(void* pvParameters) {
             }
         }
 
+		oldStateLight1 = stateLight1;
+        oldStateLight2 = stateLight2;
+
+        //ets_printf("Something before");
+
         if (lastCount != count) {
             lastCount = (int) count;
             printToDisplay(count);
+			// xTaskNotifyWait(
+			// 	0x00,
+			// 	ULONG_MAX,
+			// 	&ulNotifiedValue,
+			// 	portMAX_DELAY
+			// );
         }
+		
+		//ets_printf("Something after");
 
-        oldStateLight1 = stateLight1;
-        oldStateLight2 = stateLight2;
-
-        stateChanged = false;
     }
 }
 
-void light1Handler() {
-    // stateChanged = true;
-    // stateLight1 = gpio_get_level(LIGHT1_GPIO);
+// Version 2: you cant cross both of them at the same time
+// void showRoomState(void* pvParameters) {
+//     int lastCount = -1;
+//     bool stateLight1, stateLight2;
+//     oldStateLight1 = gpio_get_level(LIGHT1_GPIO);
+//     oldStateLight2 = gpio_get_level(LIGHT2_GPIO);
+//     while(1) {
+// 		sense the state of the lights
+//         stateLight1 = gpio_get_level(LIGHT1_GPIO);
+//         stateLight2 = gpio_get_level(LIGHT2_GPIO);
+
+// 		if(stateLight1 != lastFlickerableState1) {
+// 			lastDebounceTime1 = esp_timer_get_time();
+// 			lastFlickerableState1 = stateLight1;
+// 		}
+
+// 		if(stateLight2 != lastFlickerableState2) {
+// 			lastDebounceTime2 = esp_timer_get_time();
+// 			lastFlickerableState2 = stateLight2;
+// 		}
+
+//         if (stateLight1 != oldStateLight1 && stateLight1) {
+//             if((esp_timer_get_time() - lastDebounceTime1) > DEBOUNCE_TIME) 
+// 				countLight1++;
+// 			if(countLight1!=0 && countLight2!=0) {
+// 				count++;
+// 				countLight1--;
+// 				countLight2--;
+// 			}
+//         }
+
+//         if (stateLight2 != oldStateLight2 && stateLight2) {
+//             if((esp_timer_get_time() - lastDebounceTime2) > DEBOUNCE_TIME)
+// 				countLight2++;
+// 			if(countLight1!=0 && countLight2!=0) {
+// 				if(count>0) 
+// 					count--;	
+// 				countLight1--;
+// 				countLight2--;
+// 			}
+// 		}
+
+//         if (lastCount != count) {
+//             lastCount = (int) count;
+//             printToDisplay(count);
+//         }
+
+//         oldStateLight1 = stateLight1;
+//         oldStateLight2 = stateLight2;
+
+//     }
+// }
+
+void IRAM_ATTR light1Handler() {
+    //xTaskNotify(taskHandle, 0, eNoAction);
 }
 
-void light2Handler() {
-    // stateChanged = true;
-    // stateLight2 = gpio_get_level(LIGHT2_GPIO);
+void IRAM_ATTR light2Handler() {
+    //xTaskNotify(taskHandle, 0, eNoAction);
 }
 
 void app_main(void){
